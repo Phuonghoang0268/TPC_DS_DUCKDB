a9dbe33a9d9e4344141bc5c7aeb4118a4ec1345c
phuong <hoangvietphuong826@gmail.com>
2024-10-20 17:24:47 +0200

    fix queries (interval days)

--- generated_queries/query_19.sql
+++ generated_queries/query_19.sql
@@ -40,7 +40,7 @@ with ss_items as
                                      where d_date = '2000-02-12'))
   and ws_sold_date_sk   = d_date_sk
  group by i_item_id)
-  select  ss_items.item_id
+ select ss_items.item_id
        ,ss_item_rev
        ,ss_item_rev/((ss_item_rev+cs_item_rev+ws_item_rev)/3) * 100 ss_dev
        ,cs_item_rev
@@ -48,15 +48,16 @@ with ss_items as
        ,ws_item_rev
        ,ws_item_rev/((ss_item_rev+cs_item_rev+ws_item_rev)/3) * 100 ws_dev
        ,(ss_item_rev+cs_item_rev+ws_item_rev)/3 average
- from ss_items,cs_items,ws_items
- where ss_items.item_id=cs_items.item_id
-   and ss_items.item_id=ws_items.item_id 
-   and ss_item_rev between 0.9 * cs_item_rev and 1.1 * cs_item_rev
-   and ss_item_rev between 0.9 * ws_item_rev and 1.1 * ws_item_rev
-   and cs_item_rev between 0.9 * ss_item_rev and 1.1 * ss_item_rev
-   and cs_item_rev between 0.9 * ws_item_rev and 1.1 * ws_item_rev
-   and ws_item_rev between 0.9 * ss_item_rev and 1.1 * ss_item_rev
-   and ws_item_rev between 0.9 * cs_item_rev and 1.1 * cs_item_rev
- order by item_id
-         ,ss_item_rev
- limit 100;
+from ss_items
+join cs_items on ss_items.item_id = cs_items.item_id
+join ws_items on ss_items.item_id = ws_items.item_id
+where ss_items.item_id = cs_items.item_id
+  and ss_items.item_id = ws_items.item_id
+  and ss_item_rev between 0.9 * cs_item_rev and 1.1 * cs_item_rev
+  and ss_item_rev between 0.9 * ws_item_rev and 1.1 * ws_item_rev
+  and cs_item_rev between 0.9 * ss_item_rev and 1.1 * ss_item_rev
+  and cs_item_rev between 0.9 * ws_item_rev and 1.1 * ws_item_rev
+  and ws_item_rev between 0.9 * ss_item_rev and 1.1 * ss_item_rev
+  and ws_item_rev between 0.9 * cs_item_rev and 1.1 * cs_item_rev
+order by ss_items.item_id, ss_item_rev
+limit 100;
